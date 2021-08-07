b153444484e3a1642378a59135e4b8b68e91bef2
Igor Okulist <okigan@gmail.com>
2021-08-07 16:22:55 -0700

    add sample predicate

--- cmd/predicate/main.rs
+++ cmd/predicate/main.rs
@@ -2,21 +2,24 @@ use crepe::crepe;
 
 crepe! {
     @input
-    struct Edge(i32, i32);
+    struct User(i32);
+
+    @input
+    struct HasRole(i32, i32);
 
     @output
-    struct Reachable(i32, i32);
+    struct Allowed(i32);
 
-    Reachable(x, y) <- Edge(x, y);
-    Reachable(x, z) <- Edge(x, y), Reachable(y, z);
+    Allowed(u) <- User(u), HasRole(u, 0);
 }
 
 fn main() {
     let mut runtime = Crepe::new();
-    runtime.extend(&[Edge(1, 2), Edge(2, 3), Edge(3, 4), Edge(2, 5)]);
+    runtime.extend(&[User(1)]);
+    runtime.extend(&[HasRole(1, 0)]);
 
-    let (reachable,) = runtime.run();
-    for Reachable(x, y) in reachable {
-        println!("node {} can reach node {}", x, y);
+    let (allowed,) = runtime.run();
+    for Allowed(x) in allowed {
+        println!("User {} is allowed", x);
     }
 }
