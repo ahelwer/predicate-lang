10c495be19cb8ef73a3eb32211bdb5af980fec9c
Andrew Helwer <ahelwer@protonmail.com>
2022-03-21 16:52:22 -0400

    Defined a global policy invariant

--- policy.py
+++ policy.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass
 from teleport import Entities
-import typing
 import z3  # type: ignore
 
 
 def allows_matrix() -> z3.BoolRef:
+  """
+  Implement a basic allow matrix with Z3 expressions. Here, each user and
+  resource combination has to be explicitly enumerated if it is allowed.
+  """
   return z3.Or(
     z3.And(
       Entities.USERNAME == z3.StringVal("ahelwer"),
@@ -17,28 +19,76 @@ def allows_matrix() -> z3.BoolRef:
   )
 
 def allow_resource_attribs() -> z3.BoolRef:
+  """
+  Implement access control predicated on resource attributes.
+  """
   return z3.And(
-    Entities.resource_attribute_str("foo", "bar", "baz"),
-    Entities.resource_attribute_str("baz", "foobar")
+    Entities.resource_attribute_str_eq("foo", "bar", "baz", "boo"),
+    Entities.resource_attribute_str_eq("baz", "foobar")
   )
 
 def allow_user_attribs() -> z3.BoolRef:
+  """
+  Implement access control predicated on user attributes.
+  """
   return z3.And(
-    Entities.user_attribute_str("foo", "bar"),
-    Entities.user_attribute_str("baz", "foobar")
+    Entities.user_attribute_str_eq("foo", "bar"),
+    Entities.user_attribute_str_eq("baz", "foobar")
   )
 
 def allow_role_set() -> z3.BoolRef:
+  """
+  Implement Attribute-Based Access Control similar to to that available
+  in Teleport roles.
+  """
   return Entities.role_set({
     "access-public" : z3.And(
-      Entities.resource_attribute_str("env", "public"),
-      Entities.resource_attribute_any_str("owner")
+      Entities.resource_attribute_str_eq("env", "public"),
+      Entities.resource_attribute_str_eq_any("owner")
     ),
     "access-private" : z3.And(
-      Entities.resource_attribute_str("env", "private"),
-      Entities.resource_attribute(z3.StringVal("owner"), Entities.USERNAME)
+      Entities.resource_attribute_str_eq("env", "private"),
+      Entities.resource_attribute_eq(z3.StringVal("owner"), Entities.USERNAME)
     )
   })
+
+def local_invariant() -> z3.BoolRef:
+  """
+  Defines a "local" invariant, to be checked on each authorization
+  request. These are constraints placed on the concrete attributes
+  of a given user or resource.
+  """
+  return z3.Not(
+    z3.And(
+      Entities.USER_GROUP_MEMBERSHIP(z3.StringVal("users")),
+      Entities.USER_GROUP_MEMBERSHIP(z3.StringVal("admins"))
+    )
+  )
+
+def global_invariant() -> z3.BoolRef:
+  """
+  Defines a "global" invariant, which can be checked against the
+  set of abstract rules in the system. In this case, if the above
+  roles allow access to a user and their username is *not* listed
+  as the resource owner, then the resource must be in the public
+  environment and the user must be in the access-public group.
+  """
+  rules = allow_role_set()
+  return z3.Implies(
+    z3.And(
+      rules,
+      z3.Not(
+        Entities.resource_attribute_eq(
+          z3.StringVal("owner"),
+          Entities.USERNAME
+        )
+      )
+    ),
+    z3.And(
+      Entities.resource_attribute_str_eq("env", "public"),
+      Entities.USER_GROUP_MEMBERSHIP(z3.StringVal("access-public"))
+    )
+  )
 
 print("Matrix:")
 print(allows_matrix().sexpr())
@@ -54,4 +104,21 @@ print()
 
 print("Role Set:")
 print(allow_role_set().sexpr())
+print()
+
+print("Local Invariant:")
+print(local_invariant().sexpr())
+print()
+
+print("Global Invariant:")
+inv = global_invariant()
+print(inv.sexpr())
+solver = z3.Solver()
+solver.add(z3.Not(inv))
+result = solver.check()
+if z3.unsat == result:
+  print("Global invariant holds")
+else:
+  print("Global invariant does not hold; counterexample:")
+  print(solver.model())
 print()
--- teleport.py
+++ teleport.py
@@ -6,7 +6,7 @@ class Entities:
 
   _RESOURCE_ATTRIBUTE_KEYS: z3.FuncDeclRef = z3.Function("resource_attribute_keys", z3.StringSort(), z3.BoolSort())
   _RESOURCE_ATTRIBUTE_VALUES: z3.FuncDeclRef = z3.Function("resource_attribute_values", z3.StringSort(), z3.StringSort())
-  def resource_attribute(key: z3.SeqRef, *values: list[z3.SeqRef]) -> z3.BoolRef:
+  def resource_attribute_eq(key: z3.SeqRef, *values: list[z3.SeqRef]) -> z3.BoolRef:
     return z3.And(
       Entities._RESOURCE_ATTRIBUTE_KEYS(key),
       z3.Or([
@@ -14,21 +14,21 @@ class Entities:
       ])
     )
 
-  def resource_attribute_str(key: str, *values: list[str]) -> z3.BoolRef:
-    return Entities.resource_attribute(
+  def resource_attribute_str_eq(key: str, *values: list[str]) -> z3.BoolRef:
+    return Entities.resource_attribute_eq(
       z3.StringVal(key),
       *[z3.StringVal(value) for value in values]
     )
   
-  def resource_attribute_any(key: z3.SeqRef) -> z3.BoolRef:
+  def resource_attribute_eq_any(key: z3.SeqRef) -> z3.BoolRef:
     return Entities._RESOURCE_ATTRIBUTE_KEYS(key)
 
-  def resource_attribute_any_str(key: str) -> z3.BoolRef:
-    return Entities.resource_attribute_any(z3.StringVal(key))
+  def resource_attribute_str_eq_any(key: str) -> z3.BoolRef:
+    return Entities.resource_attribute_eq_any(z3.StringVal(key))
 
   _USER_ATTRIBUTE_KEYS: z3.FuncDeclRef = z3.Function("user_attribute_keys", z3.StringSort(), z3.BoolSort())
   _USER_ATTRIBUTE_VALUES: z3.FuncDeclRef = z3.Function("user_attribute_values", z3.StringSort(), z3.StringSort(), z3.BoolSort())
-  def user_attribute(key: z3.SeqRef, *values: list[z3.SeqRef]) -> z3.BoolRef:
+  def user_attribute_eq(key: z3.SeqRef, *values: list[z3.SeqRef]) -> z3.BoolRef:
     return z3.And(
       Entities._USER_ATTRIBUTE_KEYS(key),
       z3.Or([
@@ -36,17 +36,17 @@ class Entities:
       ])
     )
   
-  def user_attribute_str(key: str, *values: list[str]) -> z3.BoolRef:
-    return Entities.user_attribute(
+  def user_attribute_str_eq(key: str, *values: list[str]) -> z3.BoolRef:
+    return Entities.user_attribute_eq(
       z3.StringVal(key),
       *[z3.StringVal(value) for value in values]
     )
   
-  _USER_ROLE_MEMBERSHIP: z3.FuncDeclRef = z3.Function("user_group_membership", z3.StringSort(), z3.BoolSort())
+  USER_GROUP_MEMBERSHIP: z3.FuncDeclRef = z3.Function("user_group_membership", z3.StringSort(), z3.BoolSort())
   def role_set(roles: dict[str, z3.BoolRef]) -> z3.BoolRef:
     return z3.Or([
       z3.If(
-        Entities._USER_ROLE_MEMBERSHIP(z3.StringVal(role_name)),
+        Entities.USER_GROUP_MEMBERSHIP(z3.StringVal(role_name)),
         role_expr,
         z3.BoolVal(False)
       ) for role_name, role_expr in roles.items()
